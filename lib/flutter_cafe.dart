32e07740b4ab9c9ab8190ea339b8155839eada80
Moses Karunia <moseskarunia@gmail.com>
2021-04-13 21:39:19 +0700

    waffle finalization

---
+++ lib/flutter_cafe.dart
@@ -0,0 +1,3 @@
+library flutter_cafe;
+
+export 'waffle.dart';
--- lib/waffle.dart
+++ lib/waffle.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 /// Card widget to be display a complex row of a table.
