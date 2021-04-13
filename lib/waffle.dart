dfb8351e2b128bdcd13e1903f53c9e297b3f5ab7
Moses Karunia <moseskarunia@gmail.com>
2021-04-13 22:02:18 +0700

    Waffle (#1)
    * should display only columns, inside a row, and inside a card
    
    * waffle show widgets and tappable and longPressable
    
    * format docs
    
    * test title and width
    
    * add margin and padding
    
    * asssertion error
    
    * asTitleAndColumnOfIconAndCaptions
    
    * waffle finalization
    
    * changelog
    
    * gitignore, funding, flutter_cafe
    
    * remove unused import
    
    * remove uneeded ci script
    
    * add repo to pubspec

---
+++ lib/waffle.dart
@@ -0,0 +1,176 @@
+import 'package:flutter/material.dart';
+
+/// Card widget to be display a complex row of a table.
+/// A waffle is divided into several columns, each column has
+/// [WaffleTopping] as children.
+///
+/// When a waffle is long tapped, it switches into flavor editing mode.
+/// Flavor is a local marker to make complex filtering easier.
+///
+/// You can attach both [onTap] and [onLongPress] to the waffle.
+class Waffle extends StatelessWidget {
+  /// Widget to display at the top most of the [Waffle].
+  /// Usually a row of flavors
+  final Widget? header;
+
+  /// Columns of the Waffle, usually uses [WaffleTopping].
+  /// Will be placed as a row children.
+  final List<Widget> columns;
+
+  /// Widget to display at the bottom-most of the [Waffle]
+  final Widget? footer;
+
+  final void Function(BuildContext)? onTap, onLongPress;
+
+  const Waffle({
+    Key? key,
+    required this.columns,
+    this.footer,
+    this.header,
+    this.onTap,
+    this.onLongPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [];
+
+    if (header != null) {
+      children.add(header!);
+    }
+
+    children.add(Row(children: columns));
+
+    if (footer != null) {
+      children.add(footer!);
+    }
+
+    Widget card = Card(
+      child: children.length == 1
+          ? Row(children: columns)
+          : Column(children: children),
+    );
+
+    if (onTap == null && onLongPress == null) {
+      return card;
+    }
+
+    return InkWell(
+      onTap: onTap != null ? () => onTap!(context) : null,
+      onLongPress: onLongPress != null ? () => onLongPress!(context) : null,
+      child: card,
+    );
+  }
+}
+
+/// Topping is the preferred widget for [Waffle] columns
+class WaffleTopping extends StatelessWidget {
+  /// Width of the topping. Will have unbounded width if null
+  final double? width;
+
+  /// The main information of the column. Usually a bodyText2 styled text.
+  final Widget title;
+
+  /// The supporting informations of the column. Usually a column.
+  /// If using text as one of column children, it's recommended to have smaller
+  /// text size than [title]. (e.g. caption / overline)
+  final Widget? content;
+
+  final EdgeInsetsGeometry? margin, padding;
+
+  const WaffleTopping({
+    required this.title,
+    Key? key,
+    this.width,
+    this.content,
+    this.margin,
+    this.padding,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [title];
+
+    if (content != null) {
+      children.add(content!);
+    }
+
+    Widget main = children.length > 1
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: children,
+          )
+        : title;
+
+    if (width == null && padding == null && margin == null) return main;
+
+    return Container(
+      margin: margin,
+      padding: padding,
+      width: width,
+      child: main,
+    );
+  }
+
+  /// A waffle topping with title and icons and content texts.
+  /// Each [contentTexts] will be styled with caption, wrapped with Expanded,
+  /// and spaced 16 wide with the icon.
+  ///
+  /// * [titleText] - Will be styled with bodyText2
+  /// * [icons] will be paired with [contentText] based on their index. Both must
+  /// have identical length, otherwise will throw an AssertionError.
+  factory WaffleTopping.asTitleAndColumnOfIconAndCaptions({
+    required BuildContext context,
+    required String titleText,
+    required List<Widget> icons,
+    required List<String> contentTexts,
+    Key? key,
+    EdgeInsetsGeometry? margin,
+    EdgeInsetsGeometry? padding,
+    double? width,
+  }) {
+    if (icons.length != contentTexts.length) {
+      throw AssertionError(
+          'icons and contentTexts should have identical length');
+    }
+
+    Widget title = Padding(
+      padding: const EdgeInsets.only(bottom: 8),
+      child: Text(
+        titleText,
+        style: Theme.of(context).textTheme.bodyText2,
+      ),
+    );
+
+    Widget content = Column(
+      children: List<int>.generate(icons.length, (i) => i)
+          .map(
+            (i) => Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Row(
+                children: [
+                  icons[i],
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: Text(
+                      contentTexts[i],
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+          .toList(),
+    );
+
+    return WaffleTopping(
+      key: key,
+      title: title,
+      content: content,
+      margin: margin,
+      padding: padding,
+      width: width,
+    );
+  }
+}
---
+++ test/waffle_test.dart
@@ -0,0 +1,377 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cafe/flutter_cafe.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+class FakeContext extends Fake implements BuildContext {}
+
+void main() {
+  group('Waffle', () {
+    final List<Widget> columnFixtures = [
+      SizedBox(width: 100, height: 100, key: Key('1st')),
+      SizedBox(width: 100, height: 100, key: Key('2nd')),
+      SizedBox(width: 100, height: 100, key: Key('3rd')),
+      SizedBox(width: 100, height: 100, key: Key('4th')),
+    ];
+    final Widget headerFixture =
+        SizedBox(key: Key('header'), width: 200, height: 100);
+    final Widget footerFixture =
+        SizedBox(key: Key('footer'), width: 200, height: 50);
+
+    Future<void> _testColumns() async {
+      expect(find.byKey(Key('1st')), findsOneWidget);
+      expect(find.byKey(Key('2nd')), findsOneWidget);
+      expect(find.byKey(Key('3rd')), findsOneWidget);
+      expect(find.byKey(Key('4th')), findsOneWidget);
+    }
+
+    Future<void> _testHeaderAndFooter() async {
+      expect(find.byKey(Key('header')), findsOneWidget);
+      expect(find.byKey(Key('footer')), findsOneWidget);
+      expect(
+        find.byWidgetPredicate(
+          (w) =>
+              w is Card &&
+              w.child is Column &&
+              (w.child as Column).children.length == 3,
+        ),
+        findsOneWidget,
+        reason: 'Direct card child is column',
+      );
+    }
+
+    testWidgets(
+      'should display only columns, inside a row, and inside a card',
+      (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(body: Waffle(columns: columnFixtures)),
+        ));
+
+        await _testColumns();
+
+        expect(
+          find.byWidgetPredicate(
+            (w) =>
+                w is Card &&
+                w.child is Row &&
+                (w.child as Row).children.whereType<SizedBox>().length == 4,
+          ),
+          findsOneWidget,
+          reason: 'Since no other vertical child, '
+              'column row is the direct child of Card',
+        );
+
+        expect(find.byType(InkWell), findsNothing);
+      },
+    );
+
+    testWidgets(
+      'should display header and footer',
+      (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: Waffle(
+              header: headerFixture,
+              footer: footerFixture,
+              columns: columnFixtures,
+            ),
+          ),
+        ));
+
+        await _testColumns();
+        await _testHeaderAndFooter();
+
+        expect(find.byType(InkWell), findsNothing);
+      },
+    );
+
+    testWidgets('should trigger onTap and onLongPress', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: Waffle(
+            header: headerFixture,
+            footer: footerFixture,
+            columns: columnFixtures,
+            onTap: (_) => print('onTap'),
+            onLongPress: (_) => print('onLongPress'),
+          ),
+        ),
+      ));
+
+      await _testColumns();
+      await _testHeaderAndFooter();
+
+      await expectLater(
+        () async => await tester.tap(find.byType(InkWell)),
+        prints('onTap\n'),
+      );
+      await expectLater(
+        () async => await tester.longPress(find.byType(InkWell)),
+        prints('onLongPress\n'),
+      );
+    });
+  });
+
+  group('WaffleTopping', () {
+    testWidgets('should contain title', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(body: WaffleTopping(title: Text('Topping title'))),
+      ));
+
+      expect(find.text('Topping title'), findsOneWidget);
+      expect(
+        find.byType(Container),
+        findsNothing,
+        reason: 'because width is null',
+      );
+      expect(
+        find.byType(Column),
+        findsNothing,
+        reason: 'because only title is provided',
+      );
+    });
+    testWidgets('should contain title and content', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: Scaffold(
+          body: WaffleTopping(
+            title: Text('Topping title'),
+            content: Text('Topping content'),
+          ),
+        ),
+      ));
+
+      expect(find.text('Topping title'), findsOneWidget);
+      expect(find.text('Topping content'), findsOneWidget);
+
+      expect(
+        find.byWidgetPredicate(
+          (w) =>
+              w is Column && w.crossAxisAlignment == CrossAxisAlignment.start,
+        ),
+        findsOneWidget,
+        reason: 'because only caption is provided',
+      );
+    });
+    testWidgets(
+      'should be wrapped with container if width is provided',
+      (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: WaffleTopping(title: Text('Topping title'), width: 200),
+          ),
+        ));
+
+        expect(find.text('Topping title'), findsOneWidget);
+        expect(
+          find.byWidgetPredicate(
+              (w) => w is Container && w.constraints!.maxWidth == 200),
+          findsOneWidget,
+          reason: 'because width is provided',
+        );
+      },
+    );
+
+    testWidgets(
+      'should be wrapped with container if margin or padding is provided',
+      (tester) async {
+        await tester.pumpWidget(MaterialApp(
+          home: Scaffold(
+            body: WaffleTopping(
+              title: Text('Topping title'),
+              margin: const EdgeInsets.all(8),
+              padding: const EdgeInsets.all(16),
+            ),
+          ),
+        ));
+
+        expect(find.text('Topping title'), findsOneWidget);
+        expect(
+          find.byWidgetPredicate((w) =>
+              w is Container &&
+              w.padding == const EdgeInsets.all(16) &&
+              w.margin == const EdgeInsets.all(8)),
+          findsOneWidget,
+          reason: 'even if width not provied, either padding or margin is',
+        );
+      },
+    );
+
+    group('WaffleTopping.asTitleAndColumnOfIconAndCaptions', () {
+      test(
+        "should throw AssertionError when icons and contentTexts "
+        "doesn't have identical length",
+        () async {
+          await expectLater(
+            () => WaffleTopping.asTitleAndColumnOfIconAndCaptions(
+                context: FakeContext(),
+                titleText: 'abc',
+                icons: [Icon(Icons.person)],
+                contentTexts: []),
+            throwsA(isA<AssertionError>()),
+          );
+        },
+      );
+
+      testWidgets(
+        'should create WaffleTopping with title text John Doe, '
+        'and 2 content rows: Icons.email - johndoe@test.com & '
+        'Icons.home - Sesame Street 123 & Icons.cake - 1st Jan 21',
+        (tester) async {
+          await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+              body: Builder(builder: (context) {
+                return WaffleTopping.asTitleAndColumnOfIconAndCaptions(
+                  context: context,
+                  titleText: 'John Doe',
+                  icons: [
+                    Icon(Icons.email),
+                    Icon(Icons.home),
+                    Icon(Icons.cake),
+                  ],
+                  contentTexts: [
+                    'johndoe@test.com',
+                    'Sesame Street 123',
+                    '1st Jan 21',
+                  ],
+                );
+              }),
+            ),
+          ));
+          expect(
+            find.byWidgetPredicate(
+              (w) =>
+                  w is Padding &&
+                  w.child is Text &&
+                  (w.child as Text).data == 'John Doe' &&
+                  w.padding == const EdgeInsets.only(bottom: 8),
+            ),
+            findsOneWidget,
+            reason: 'Title is John Doe and has bottom padding 8',
+          );
+          expect(
+            find.byWidgetPredicate(
+              (w) =>
+                  w is Text &&
+                  w.data == 'John Doe' &&
+                  w.style!.fontSize == 14 &&
+                  w.style!.fontWeight == FontWeight.w400,
+            ),
+            findsOneWidget,
+            reason: 'Title styled with bodyText2',
+          );
+
+          expect(
+            find.byWidgetPredicate(
+              (w) => w is Column && w.children.length == 3,
+            ),
+            findsOneWidget,
+            reason: 'A column containing 3 children',
+          );
+
+          expect(
+            find.byWidgetPredicate(
+              (w) =>
+                  w is Row &&
+                  w.children.isNotEmpty &&
+                  w.children[0] is Icon &&
+                  (w.children[0] as Icon).icon == Icons.email &&
+                  w.children[1] is SizedBox &&
+                  (w.children[1] as SizedBox).width == 16 &&
+                  w.children[2] is Expanded &&
+                  ((w.children[2] as Expanded).child as Text).data ==
+                      'johndoe@test.com' &&
+                  ((w.children[2] as Expanded).child as Text).style!.fontSize ==
+                      12 &&
+                  ((w.children[2] as Expanded).child as Text)
+                          .style!
+                          .fontWeight ==
+                      FontWeight.w400,
+            ),
+            findsOneWidget,
+            reason: 'A row containing Icons.email & 16-width sized box & '
+                'caption-styled text',
+          );
+
+          expect(
+            find.byWidgetPredicate(
+              (w) =>
+                  w is Row &&
+                  w.children.isNotEmpty &&
+                  w.children[0] is Icon &&
+                  (w.children[0] as Icon).icon == Icons.home &&
+                  w.children[1] is SizedBox &&
+                  (w.children[1] as SizedBox).width == 16 &&
+                  w.children[2] is Expanded &&
+                  ((w.children[2] as Expanded).child as Text).data ==
+                      'Sesame Street 123' &&
+                  ((w.children[2] as Expanded).child as Text).style!.fontSize ==
+                      12 &&
+                  ((w.children[2] as Expanded).child as Text)
+                          .style!
+                          .fontWeight ==
+                      FontWeight.w400,
+            ),
+            findsOneWidget,
+            reason: 'A row containing Icons.home & 16-width sized box & '
+                'caption-styled text',
+          );
+
+          expect(
+            find.byWidgetPredicate(
+              (w) =>
+                  w is Row &&
+                  w.children.isNotEmpty &&
+                  w.children[0] is Icon &&
+                  (w.children[0] as Icon).icon == Icons.cake &&
+                  w.children[1] is SizedBox &&
+                  (w.children[1] as SizedBox).width == 16 &&
+                  w.children[2] is Expanded &&
+                  ((w.children[2] as Expanded).child as Text).data ==
+                      '1st Jan 21' &&
+                  ((w.children[2] as Expanded).child as Text).style!.fontSize ==
+                      12 &&
+                  ((w.children[2] as Expanded).child as Text)
+                          .style!
+                          .fontWeight ==
+                      FontWeight.w400,
+            ),
+            findsOneWidget,
+            reason: 'A row containing Icons.cake & 16-width sized box & '
+                'caption-styled text',
+          );
+        },
+      );
+
+      testWidgets(
+        'should find margin, padding and width',
+        (tester) async {
+          await tester.pumpWidget(MaterialApp(
+            home: Scaffold(
+              body: Builder(builder: (context) {
+                return WaffleTopping.asTitleAndColumnOfIconAndCaptions(
+                  context: context,
+                  titleText: 'John Doe',
+                  icons: [Icon(Icons.email)],
+                  contentTexts: ['johndoe@test.com'],
+                  margin: const EdgeInsets.all(17),
+                  padding: const EdgeInsets.all(19),
+                  width: 102,
+                );
+              }),
+            ),
+          ));
+
+          await tester.pumpAndSettle();
+
+          expect(
+            find.byWidgetPredicate((w) =>
+                w is Container &&
+                w.constraints!.maxWidth == 102 &&
+                w.margin == const EdgeInsets.all(17) &&
+                w.padding == const EdgeInsets.all(19)),
+            findsOneWidget,
+          );
+        },
+      );
+    });
+  });
+}
